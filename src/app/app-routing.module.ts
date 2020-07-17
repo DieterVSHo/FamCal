27b2a5fd528d02906cd091c755450d85946b5dde
dietervs <dieter.vsx@gmail.com>
2020-07-17 17:05:05 +0200

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FamilyCalendarComponent } from './family-calendar/family-calendar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'familycalendar', component: FamilyCalendarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FamilyCalendarComponent } from './family-calendar/family-calendar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FamilyCalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

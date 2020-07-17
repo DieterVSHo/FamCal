299c6192ea588fcdec3cc408df23a90f598870b6
dietervs <dieter.vsx@gmail.com>
2020-07-17 17:55:37 +0200

    added cal plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 import { FamilyCalendarComponent } from './family-calendar/family-calendar.component';
 
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -18,6 +26,7 @@ import { FamilyCalendarComponent } from './family-calendar/family-calendar.compo
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FullCalendarModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/family-calendar/family-calendar.component.ts
+++ src/app/family-calendar/family-calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-family-calendar',
@@ -9,7 +10,12 @@ export class FamilyCalendarComponent implements OnInit {
 
   constructor() { }
 
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth'
+  };
+
   ngOnInit(): void {
+    
   }
 
 }
